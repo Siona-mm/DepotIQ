9a843be7a15a20ee3fa6cc808e3b7fa079cd50eb
siona <sionamustafa2000@gmail.com>
2026-07-19 18:04:23 +0200

    Create product service layer

--- backend/src/main/java/com/depotiq/repositories/ProductRepository.java
+++ backend/src/main/java/com/depotiq/repositories/ProductRepository.java
@@ -2,9 +2,12 @@ package com.depotiq.repositories;
 
 import com.depotiq.models.Product;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByProductCode(String productCode);
+
+    boolean existsByProductCode(String productCode);
 }
