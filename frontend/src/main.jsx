42b56eedf9f66a846f746e79d43542aded3422bd
siona <sionamustafa2000@gmail.com>
2026-07-28 22:08:45 +0200

    Load Geist variable font

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "@fontsource-variable/geist/wght.css";
 import App from "./App.jsx";
 import "./styles/main.css";
 
@@ -8,4 +9,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
-
